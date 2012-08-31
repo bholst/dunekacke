f4b7af8a70976acc82e82037443d1cae99ff528e
Bastian Holst <bastianholst@gmx.de>
2012-08-31 17:41:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dunekacke::Application.config.secret_token = 'b4677b0ffd78e8128dd7a630e5339fc3b8670a63e40dad51e61af66ac7d062f0572a68db22ac6815a15da20539625bba362018bb0207e2662e05e93ab5cc7f7d'
