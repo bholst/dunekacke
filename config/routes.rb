51af1834ac539a62b7e9dfb456493bdc44e62501
Bastian Holst <bastianholst@gmx.de>
2012-09-01 21:43:55 +0200

    Adding user signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Dunekacke::Application.routes.draw do
+  get "users/new"
+
   # You can have the root of your site routed with "root"
   root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
   
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
