ccb3c031b1d03483e0b79d1ac536b15350da084d
Bastian Holst <bastianholst@gmx.de>
2012-09-01 13:14:09 +0200

    Titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_content('Dunekacke')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Dunekacke | Home")
+    end
   end
   
   describe "Help page" do
@@ -13,12 +18,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Dunekacke | Help")
+    end
   end
   
   describe "About page" do
     it "should have the content 'About Dunekacke'" do
       visit '/static_pages/about'
       page.should have_content('About Dunekacke')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Dunekacke | About")
     end
   end
 end
