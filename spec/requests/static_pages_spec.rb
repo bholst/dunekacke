0687b272f906976a7e14bd7e0cd94f652cb4bb94
Bastian Holst <bastianholst@gmx.de>
2012-09-01 16:21:47 +0200

    Enabling links.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,42 +3,55 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Dunekacke'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Dunekacke')
     end
     
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "Dunekacke")
     end
     
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => "|")
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "Dunekacke | Help")
     end
   end
   
   describe "About page" do
     it "should have the content 'About Dunekacke'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Dunekacke')
     end
     
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => "Dunekacke | About")
+    end
+  end
+  
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', :text => "Dunekacke | Contact")
     end
   end
 end
