6aa132b8e6b2821e8c4e76d56ea0330ea594d7a8
Bastian Holst <bastianholst@gmx.de>
2012-09-02 14:11:02 +0200

    Login with invalid information.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Dunekacke::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   root to: 'static_pages#home'
   
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
