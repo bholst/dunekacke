8e1573f373ff6e362a53ddda0ba97939a590e416
Bastian Holst <bastianholst@gmx.de>
2012-09-03 21:34:55 +0200

    Finish user edit, update, index and destroy actions.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,15 @@ Spork.prefork do
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
   end
+    
+  RSpec::Matchers.define :be_accessible do |attribute|
+    match do |response|
+      response.class.accessible_attributes.include?(attribute)
+    end
+    description { "be accessible :#{attribute}" }
+    failure_message_for_should { ":#{attribute} should be accessible" }
+    failure_message_for_should_not { ":#{attribute} should not be accessible" }
+  end
 end
 
 Spork.each_run do
