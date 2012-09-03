52aa84fa816d2793a0379c405b16fc2f86700d16
Bastian Holst <bastianholst@gmx.de>
2012-09-03 11:22:21 +0200

    User login and logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign in the user
+      sign_in user
+      redirect_to user
     else
-      flash[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
   
   def destroy
+    sign_out
+    redirect_to root_url
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,6 +40,14 @@ describe "User Pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before do
+          click_button submit
+        end
+        
+        it { should have_link('Sign out') }
+      end
     end
   end
 end
