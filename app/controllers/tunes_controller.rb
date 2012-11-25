68bd15bf408f4dc75a62a29ee7e7f29af8d35342
Bastian Holst <bastianholst@gmx.de>
2012-11-25 01:11:46 +0100

    Adding tune edit.

--- app/controllers/tunes_controller.rb
+++ app/controllers/tunes_controller.rb
@@ -1,5 +1,6 @@
 class TunesController < ApplicationController
   before_filter :signed_in_user
+  before_filter :correct_user, :only => [:edit, :update]
   
   def index
     @tunes = Tune.all
@@ -13,6 +14,19 @@ class TunesController < ApplicationController
     @tune = @current_user.tunes.new 
   end
   
+  def edit
+  end
+  
+  def update
+    if @tune.update_attributes(params[:tune])
+      flash[:success] = "Profile updated"
+    
+      redirect_to @tune
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @tune = current_user.tunes.build(:content => params[:tune][:content])
     if @tune.save
@@ -22,4 +36,11 @@ class TunesController < ApplicationController
       render 'new'
     end
   end
+  
+  private
+    def correct_user
+      @tune = Tune.find(params[:id])
+      @user = @tune.user
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
